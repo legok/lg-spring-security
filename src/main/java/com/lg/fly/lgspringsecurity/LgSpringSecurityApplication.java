83fb4f70c9c27c8ed280cc4a4e6e78e7120c228c
le.gao <gaole.000@163.com>
2022-10-27 22:43:14 +0800

    Initial commit

---
+++ src/main/java/com/lg/fly/lgspringsecurity/LgSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.lg.fly.lgspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LgSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lg/fly/lgspringsecurity/LgSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lg.fly.lgspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LgSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
